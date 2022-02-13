d8026bbca411e85de21a1299fcd9166df59d627f
knightbreaker <aouledissayousri@gmail.com>
2022-02-13 21:04:10 +0100

    production-mode4

--- starTeract/settings.py
+++ starTeract/settings.py
@@ -71,7 +71,6 @@ MIDDLEWARE = [
     
 ]
 
-DISABLE_COLLECTSTATIC=1
 
 ROOT_URLCONF = 'starTeract.urls'
 
@@ -151,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
